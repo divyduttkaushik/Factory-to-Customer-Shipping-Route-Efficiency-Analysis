6359c539081543c1530c0493bfd49aecfa73d986
Divy Dutt Kaushik <divyduttkaushik@gmail.com>
2026-07-22 10:48:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(page_title="Nassau Candy Shipping Analysis", layout="wide")
+
+st.title("Factory-to-Customer Shipping Route Efficiency Analysis")
+
+st.write("Power BI project analyzing shipping efficiency and profitability.")
+
+st.header("Executive Overview")
+st.image("Screenshot 2026-07-22 102354.png")
+
+st.header("Shipping Efficiency Analysis")
+st.image("Screenshot 2026-07-22 102409.png")
+
+st.header("Product & Profitability Analysis")
+st.image("Screenshot 2026-07-22 102422.png")
